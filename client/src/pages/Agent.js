0feb977b09b736ebda7860e0c1bad364a108cea3
JackYouk <jackyoukstetter1@gmail.com>
2022-12-13 22:02:41 -0800

    added stepper for booking & changed calendly to a modal

--- client/src/pages/Agent.js
+++ client/src/pages/Agent.js
@@ -8,12 +8,15 @@ import TwitterIcon from '@mui/icons-material/Twitter';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import YouTubeIcon from '@mui/icons-material/YouTube';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
+import SchoolIcon from '@mui/icons-material/School';
+import BusinessIcon from '@mui/icons-material/Business';
+import MilitaryTechIcon from '@mui/icons-material/MilitaryTech';
 import { useState } from "react";
 
 
-import { InlineWidget } from "react-calendly";
+import { InlineWidget, PopupModal } from "react-calendly";
 import { ADD_REVIEW } from "../utils/mutations";
-
+import BookingComponent from "../components/BookingComponent";
 
 const Agent = () => {
     const paramId = useParams()['*'];
@@ -66,105 +69,135 @@ const Agent = () => {
         }
     }
 
+    const [modalState, setModalState] = useState(false);
+
     return (
-        <div style={{ maxWidth: '100vw', }}>
+        <div style={{}}>
             <ResponsiveAppBar />
-            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', margin: '8vh' }}>
+            <div style={{ display: 'flex', justifyContent: 'left', alignItems: 'center', margin: '8vh' }}>
                 {loading ? (
                     <Box sx={{ display: 'flex' }}>
                         <CircularProgress />
                     </Box>
                 ) : (
-                    <div style={{ display: 'flex', justifyContent: 'center', width: '100vw' }}>
-                        <Grid container display='flex' justifyContent='center' style={{ width: '100vw' }}>
-                            <Grid item sm={10} md={10} xl={6}>
-                                <div style={{ display: 'flex', justifyContent: 'center', flexDirection: 'column', marginTop: '50px' }}>
-                                    <div style={{ display: 'flex', alignItems: 'center', width: '100vw' }}>
-                                        <img src={data.agent.imgPath} alt={'picture of ' + data.agent.name} style={{ width: '150px', height: '150px', borderRadius: '50%' }} />
-                                        <div style={{ margin: '20px', display: 'flex', flexDirection: 'column' }}>
-                                            <h1 style={{ fontSize: '30px', fontWeight: 'bold' }}>{data.agent.name}</h1>
-                                            <h2 style={{ fontSize: '20px' }}>{data.agent.industries}</h2>
-                                            <div>
-                                                {data.agent.instagram ? (<a href={data.agent.instagram} style={{ color: '#D5AD6D' }} target='_blank'><InstagramIcon /></a>) : (<></>)}
-                                                {data.agent.instagram ? (<a href={data.agent.instagram} style={{ color: '#D5AD6D' }} target='_blank'><TwitterIcon /></a>) : (<></>)}
-                                                {data.agent.instagram ? (<a href={data.agent.instagram} style={{ color: '#D5AD6D' }} target='_blank'><LinkedInIcon /></a>) : (<></>)}
-                                            </div>
-                                            <div style={{ display: 'flex', alignItems: 'center', marginTop: '4px' }}>
-                                                ({data.agent.rating || '5.0'})
-                                                <Rating name="rating" value={Number(data.agent.rating) || 5.0} readOnly />
-                                            </div>
-                                        </div>
+                    <div style={{ display: 'flex', justifyContent: 'left', }}>
+                        <div style={{ display: 'flex', alignItems: 'left', flexDirection: 'column', marginTop: '50px' }}>
+                            <div style={{ display: 'flex', alignItems: 'center', }}>
+                                <img src={data.agent.imgPath} alt={'picture of ' + data.agent.name} style={{ width: '150px', height: '150px', borderRadius: '50%' }} />
+                                <div style={{ margin: '20px', display: 'flex', flexDirection: 'column' }}>
+                                    <h1 style={{ fontSize: '30px', fontWeight: 'bold' }}>{data.agent.name}</h1>
+                                    <h2 style={{ fontSize: '20px' }}>{data.agent.industries}</h2>
+                                    <div>
+                                        {data.agent.instagram ? (<a href={data.agent.instagram} style={{ color: '#D5AD6D' }} target='_blank'><InstagramIcon /></a>) : (<></>)}
+                                        {data.agent.instagram ? (<a href={data.agent.instagram} style={{ color: '#D5AD6D' }} target='_blank'><TwitterIcon /></a>) : (<></>)}
+                                        {data.agent.instagram ? (<a href={data.agent.instagram} style={{ color: '#D5AD6D' }} target='_blank'><LinkedInIcon /></a>) : (<></>)}
                                     </div>
-                                    <div style={{ display: 'flex', flexDirection: 'column', marginTop: '20px' }}>
-                                        <h2 style={{ fontSize: '25px' }}>Bio</h2>
-                                        <p style={{ fontSize: '15px', maxWidth: '800px' }}>{data.agent.bio}</p>
+                                    <div style={{ display: 'flex', alignItems: 'center', marginTop: '4px' }}>
+                                        ({data.agent.rating || '5.0'})
+                                        <Rating name="rating" value={Number(data.agent.rating) || 5.0} readOnly />
                                     </div>
-                                    <div style={{ display: 'flex', flexDirection: 'column', marginTop: '20px' }}>
-                                        <h2 style={{ fontSize: '25px' }}>Acheivements</h2>
-                                        <p style={{ fontSize: '15px', maxWidth: '800px' }}>{data.agent.acheivements}</p>
+                                </div>
+                            </div>
+                            <div style={{ display: 'flex', flexDirection: 'column', marginTop: '20px' }}>
+                                <h2 style={{ fontSize: '25px' }}>Bio</h2>
+                                <p style={{ fontSize: '15px', maxWidth: '800px' }}>{data.agent.bio}</p>
+                            </div>
+                            <div style={{ display: 'flex', flexDirection: 'column', marginTop: '20px' }}>
+                                <h2 style={{ fontSize: '25px' }}>Acheivements</h2>
+                                {/* <p style={{ fontSize: '15px', maxWidth: '800px' }}>{data.agent.acheivements}</p> */}
+                                <div style={{ display: 'flex', flexDirection: 'row', }}>
+                                    <SchoolIcon />
+                                    <div style={{ display: 'flex', flexDirection: 'row', marginLeft: '2px', alignItems: 'center'}}>
+                                        <p style={{ fontSize: '15px', marginLeft: '2px',}}>Harvard University</p>
+                                        {/* <p style={{ fontSize: '10px', marginLeft: '2px',}}>MS, Economics</p> */}
                                     </div>
-                                    <div style={{ display: 'flex', flexDirection: 'column', marginTop: '20px' }}>
-                                        <h2 style={{ fontSize: '25px' }}>Reviews</h2>
-                                        {data.agent.reviews.map(review => {
-                                            return (
-                                                <div style={{ display: 'flex', flexDirection: 'row' }}>
-                                                    <p style={{ fontSize: '15px', marginRight: '1px' }}><span style={{ fontWeight: 'bold' }}>{review.username}:</span> {review.review}</p>
-                                                    {review.rating ? (<Rating name="review rating" value={Number(review.rating) || 5.0} readOnly />) : (<></>)}
-                                                </div>
-                                            )
-                                        })}
-                                        {
-                                            showAddReview ? (
-                                                <div style={{ display: 'flex', flexDirection: 'column', }}>
-                                                    <TextField
-                                                        sx={{ width: '230px', marginBottom: '5px' }}
-                                                        label="Name"
-                                                        name="username"
-                                                        type='text'
-                                                        onChange={handleChange}
-                                                        variant="standard"
-                                                        size="small"
-                                                    />
-                                                    <TextField
-                                                        sx={{ width: '230px', marginBottom: '10px', }}
-                                                        label="Review"
-                                                        name="review"
-                                                        type='text'
-                                                        onChange={handleChange}
-                                                        variant="standard"
-                                                        size="small"
-                                                        multiline
-                                                        maxRows={4}
-                                                    />
-                                                    <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
-                                                        <Rating name="rating" onChange={handleChange} sx={{ marginRight: '10px' }} value={addReviewFormState.rating} />
-                                                        <Button variant="outlined" sx={{ width: '100px', }} onClick={submitReview}>Add</Button>
-                                                    </div>
+                                </div>
+                                <div style={{ display: 'flex', flexDirection: 'row', }}>
+                                    <BusinessIcon />
+                                    <div style={{ display: 'flex', flexDirection: 'row', marginLeft: '2px', alignItems: 'center'}}>
+                                        <p style={{ fontSize: '15px', marginLeft: '2px',}}>Vortex Ventures</p>
+                                        {/* <p style={{ fontSize: '10px', marginLeft: '2px',}}>MS, Economics</p> */}
+                                    </div>
+                                </div>
+                                <div style={{ display: 'flex', flexDirection: 'row', }}>
+                                    <MilitaryTechIcon />
+                                    <div style={{ display: 'flex', flexDirection: 'row', marginLeft: '2px', alignItems: 'center'}}>
+                                        <p style={{ fontSize: '15px', marginLeft: '2px',}}>Voted #1 E-commerce Agent by Forbes</p>
+                                        {/* <p style={{ fontSize: '10px', marginLeft: '2px',}}>MS, Economics</p> */}
+                                    </div>
+                                </div>
+                            </div>
 
-                                                </div>
-                                            ) : (
-                                                <a style={{color: '#D5AD6D', fontStyle: 'italic', textDecoration: 'underline', cursor: 'pointer'}} onClick={() => setShowAddReview(true)}>Add a review</a>
-                                            )
-                                        }
-                                    </div>
-                                </div>
-                            </Grid>
-                            <Grid item sm={10} md={10} xl={4} style={{ display: 'flex', justifyContent: 'center', }}>
-                                <div>
-                                    <InlineWidget
-                                        url="https://calendly.com/jackyoukstetter1/test"
-                                        pageSettings={{
-                                            backgroundColor: 'ffffff',
-                                            hideEventTypeDetails: false,
-                                            hideLandingPageDetails: false,
-                                            primaryColor: 'D5AD6D',
-                                            textColor: 'black'
-                                        }}
-                                        styles={{ width: '90vw', height: '100vh', margin: '0', padding: '0' }}
-                                    />
-                                </div>
-                            </Grid>
-                        </Grid>
+                            {/* scheduling */}
+                            <div style={{ display: 'flex', flexDirection: 'column', marginTop: '20px' }}>
+                                <h2 style={{ fontSize: '25px' }}>Book A Meeting</h2>
+                                <BookingComponent setModal={setModalState}/>
+                            </div>
+                            
+                            <div style={{ display: 'flex', flexDirection: 'column', marginTop: '20px' }}>
+                                <h2 style={{ fontSize: '25px' }}>Reviews</h2>
+                                {data.agent.reviews.map(review => {
+                                    return (
+                                        <div style={{ display: 'flex', flexDirection: 'row' }}>
+                                            <p style={{ fontSize: '15px', marginRight: '1px' }}><span style={{ fontWeight: 'bold' }}>{review.username}:</span> {review.review}</p>
+                                            {review.rating ? (<Rating name="review rating" value={Number(review.rating) || 5.0} readOnly />) : (<></>)}
+                                        </div>
+                                    )
+                                })}
+                                {
+                                    showAddReview ? (
+                                        <div style={{ display: 'flex', flexDirection: 'column', }}>
+                                            <TextField
+                                                sx={{ width: '230px', marginBottom: '5px' }}
+                                                label="Name"
+                                                name="username"
+                                                type='text'
+                                                onChange={handleChange}
+                                                variant="standard"
+                                                size="small"
+                                            />
+                                            <TextField
+                                                sx={{ width: '230px', marginBottom: '10px', }}
+                                                label="Review"
+                                                name="review"
+                                                type='text'
+                                                onChange={handleChange}
+                                                variant="standard"
+                                                size="small"
+                                                multiline
+                                                maxRows={4}
+                                            />
+                                            <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+                                                <Rating name="rating" onChange={handleChange} sx={{ marginRight: '10px' }} value={addReviewFormState.rating} />
+                                                <Button size='small' color='secondary' variant="outlined" sx={{ width: '100px', }} onClick={submitReview}>Add</Button>
+                                            </div>
+
+                                            
+                                        </div>
+                                    ) : (
+                                        <a style={{ color: '#D5AD6D', fontStyle: 'italic', textDecoration: 'underline', cursor: 'pointer' }} onClick={() => setShowAddReview(true)}>Add a review</a>
+                                    )
+                                }
+                            </div>
+                        </div>
+
+
+                        
+                        <PopupModal
+                            url="https://calendly.com/jackyoukstetter1/test"
+                            pageSettings={{
+                                backgroundColor: 'ffffff',
+                                hideEventTypeDetails: false,
+                                hideLandingPageDetails: false,
+                                primaryColor: 'D5AD6D',
+                                textColor: 'black'
+                            }}
+                            // utm={{}}
+                            // prefill={{}}
+                            onModalClose={() => setModalState(false)}
+                            open={modalState}
+                            rootElement={document.getElementById("root")}
+                        />
                     </div>
                 )}
             </div>
