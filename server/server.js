abab43434bf04b470927944544cfd095c706905d
JackYouk <jackyoukstetter1@gmail.com>
2022-12-19 20:07:11 -0800

    server fix

--- server/server.js
+++ server/server.js
@@ -25,6 +25,9 @@ if (process.env.NODE_ENV === 'production') {
   app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build/index.html'));
   });
+  app.all('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
 }
 
 
