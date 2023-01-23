e0463b2c4adf4b31a07872670c61cb5be4764b9f
JackYouk <jackyoukstetter1@gmail.com>
2023-01-22 19:35:04 -0800

    ui changes

--- client/src/components/AccountNav.js
+++ client/src/components/AccountNav.js
@@ -11,7 +11,9 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
+import blankprofile from '../images/blankprofile.png'
+import AuthService from '../utils/auth.js'
+import { Link } from 'react-router-dom';
 
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
@@ -27,35 +29,50 @@ function AccountNav() {
   };
 
   return (
-          <Box sx={{flexGrow: 0}}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} >
-                <Avatar sx={{ p: 0, width: '6vh', height: '6vh', }} className='goldBg'>JY</Avatar>
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: '8vh'}}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
+    <Box sx={{ flexGrow: 0, margin: 2 }}>
+      <Tooltip title="Open settings">
+        <IconButton onClick={handleOpenUserMenu} >
+          <Avatar sx={{ p: 1, width: '6vh', height: '6vh', backgroundColor: 'black' }} ><img style={{ width: '6vh' }} src={blankprofile} /></Avatar>
+        </IconButton>
+      </Tooltip>
+      <Menu
+        sx={{ mt: '8vh' }}
+        id="menu-appbar"
+        anchorEl={anchorElUser}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        keepMounted
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        open={Boolean(anchorElUser)}
+        onClose={handleCloseUserMenu}
+      >
+        {/* {settings.map((setting) => (
                 <MenuItem key={setting} onClick={handleCloseUserMenu}>
                   <Typography textAlign="center">{setting}</Typography>
                 </MenuItem>
-              ))}
-            </Menu>
-          </Box>
+              ))} */}
+
+        {AuthService.loggedIn() ? (
+          <MenuItem onClick={() => {
+            handleCloseUserMenu()
+            AuthService.logout()
+          }}>
+            <Typography textAlign="center">Signout</Typography>
+          </MenuItem>
+        ) : (
+          <Link to='/login'>
+            <MenuItem onClick={handleCloseUserMenu}>
+              <Typography textAlign="center">Login</Typography>
+            </MenuItem>
+          </Link>
+          )}
+      </Menu>
+    </Box>
   );
 }
 export default AccountNav;
--- client/src/pages/Platform.js
+++ client/src/pages/Platform.js
@@ -1,6 +1,7 @@
 // imports
 import '../components/platform.css'
 import vortexlogo from '../components/vortexlogo.png'
+import simplelogo from '../images/simplelogo.png'
 import { Link } from 'react-router-dom';
 import RegexAgents from '../components/RegexAgents';
 import { useState } from 'react';
@@ -9,43 +10,29 @@ import { useQuery } from '@apollo/client';
 import { QUERY_REGEX_AGENTS } from '../utils/queries';
 import AccountNav from '../components/AccountNav';
 import CheckboxesGroup from '../components/Checkbox';
+import SearchIcon from '@mui/icons-material/Search';
 
 const Home = () => {
     const [filterState, setFilterState] = useState('');
-    const { loading, data, refetch } = useQuery(QUERY_REGEX_AGENTS, {
-        variables: { key: filterState }
+    const {loading, data, refetch} = useQuery(QUERY_REGEX_AGENTS, {
+        variables: {key: filterState}
     });
 
     return (
         <>
-            {/* <nav>
-                <div class="logo">
-                    <img src={vortexlogo} alt="Logo" />
-                </div>
-                <ul>
-                    <li><a href="#">Button 1</a></li>
-                    <li><a href="#">Button 2</a></li>
-                    <li><a href="#">Button 3</a></li>
-                    <li><a href="#">Button 4</a></li>
-                    <li><a href="#">Button 5</a></li>
-                </ul>
-                <form>
-                    <input type="text" placeholder="Search" />
-                        <button type="submit">Search</button>
-                    
-                </form>
-            </nav> */}
-            <div style={{minHeight: '95vh', backgroundColor: 'white'}}>
-            <div style={{minHeight: '15vh', backgroundColor: 'white', display: 'flex', alignItems: 'center', justifyContent: 'space-between'}}>
-                <Link to='/'><img src={vortexlogo} alt='vive logo' style={{margin: '20px', maxHeight: '13vh', maxWidth: '22vw'}} /></Link>
-                <input style={{marginLeft: '7vw', minWidth: '40vw'}} className='searchBar' placeholder='Search' onChange={(event) => {
+        <div style={{minHeight: '95vh', backgroundColor: 'white'}}>
+            <div style={{minHeight: '15vh', backgroundColor: 'white', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+                <Link to='/'><img src={simplelogo} alt='vive logo' style={{margin: '20px', maxHeight: '13vh', maxWidth: '22vw'}} /></Link>
+                <input style={{}} className='searchBar' placeholder="" onChange={(event) => {
                     
                     setFilterState(event.target.value.replace(/[\\"']/g, '\\$&').replace(/\u0000/g, '\\0').replace(/\[/g, "&#91;"));
                     refetch();
                 }} />
-                <div style={{width: '40vw', display: 'flex', justifyContent: 'end', marginRight: '2vw'}}>
+                {/* <div className='tagBtn'>Hey</div> */}
+                <AccountNav/>
+                {/* <div style={{width: '40vw', display: 'flex', justifyContent: 'end', marginRight: '2vw'}}>
                     <AccountNav />
-                </div>
+                </div> */}
             </div>
             <div style={{minHeight: '80vh', backgroundColor: 'white', display: 'flex', justifyContent: 'center'}}>
                 <div style={{minHeight: '80vh', width: '100vw',}}>
@@ -53,7 +40,7 @@ const Home = () => {
                 </div>
             </div>
         </div>
-            {/* <div style={{backgroundColor: 'black', display: 'flex', flexDirection: 'column', alignItems: 'center', minHeight: '95vh'}}>
+        {/* <div style={{backgroundColor: 'black', display: 'flex', flexDirection: 'column', alignItems: 'center', minHeight: '95vh'}}>
             <Link to='/'><img src={vortexlogo} alt='vive logo' style={{margin: '20px'}} /></Link>
             <input placeholder='Search' onChange={(event) => {
                 setFilterState(event.target.value);
